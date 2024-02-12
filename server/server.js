ae0cfad8513e543917f1ea69888739f28f7c7e6d
ojas01010 <ojas01010@gmail.com>
2024-02-12 10:03:06 +0530

    update

--- server/server.js
+++ server/server.js
@@ -9,7 +9,12 @@ const port = process.env.PORT || 9000;
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 const url = process.env.MY_MONGO_URL;
 
 // Database
